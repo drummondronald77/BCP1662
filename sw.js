6190c0c573db187b329615169c6df59c1c7e06c2
drummondronald77 <drummond.ronald@gmail.com>
2026-09-15 18:20:27 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='bcp1662-v1';const A=['./','index.html','styles.css','app.js','data.js','manifest.webmanifest','icon-192.png','icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
